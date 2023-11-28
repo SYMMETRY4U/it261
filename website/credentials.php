bc156948eed26e02dab45ebc5ecb9e6d368ee9a3
SYMMETRY4U <121978473+SYMMETRY4U@users.noreply.github.com>
2023-11-27 22:15:00 -0800

    Delete website/credentials.php

--- website/credentials.php
+++
@@ -1,6 +0,0 @@
-<?php   // connect to database
-
-define('DB_NAME', 'symmet6_fall2023');
-define('DB_USER', 'symmet6_fall2023_user');
-define('DB_PASSWORD', '2679@$School');
-define('DB_HOST', 'localhost');
