e258aa1ec6d1a1e1bae2f56c4a049175cdc625f1
SYMMETRY4U <121978473+SYMMETRY4U@users.noreply.github.com>
2023-12-03 01:19:14 -0800

    week10a

--- weeks/week9/login.php
+++ weeks/week9/login.php
@@ -6,7 +6,7 @@ include('includes/header.php');
 <div id="wrapper">
     <h1 class="center">Our Login Page</h1>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-        <fieldset>
+        <!-- <fieldset> -->
             <label>Username</label>
             <input type="text" name="username" value="<?php if(isset($_POST['username'])) echo $_POST['username']  ;?>">
             
@@ -15,7 +15,7 @@ include('includes/header.php');
             
             <button type="submit" name="login_user" class="btn"> Login</button>
             <button type="button" onclick="window.location.href='<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>'">Reset</button>
-        </fieldset>
+        <!-- </fieldset> -->
         <?php include('errors.php'); ?>
     </form>
     <h3>Not a member yet?</h3>
